1ae52d7be242c390d4151dc3c3230e157e0a3919
kalibianw <kalibianw@gmail.com>
2022-04-20 00:50:00 +0900

    Update anno_preprocessing.py

--- anno_preprocessing.py
+++ anno_preprocessing.py
@@ -8,7 +8,7 @@ dm = AnnoDataModule(dataset_dir_path=DATASET_DIR_PATH)
 
 img_fnames, imgs = dm.img_to_np(rescaling_ratio=RESCALING_RATIO)
 print(img_fnames.shape, imgs.shape)
-np.savez_compressed(file=f"npz/AnnoImgNumpy_{RESCALING_RATIO}.npz", fname=img_fnames, imgs=imgs)
+np.savez_compressed(file=f"npz/AnnoImgNumpy_{RESCALING_RATIO}.npz", fnames=img_fnames, imgs=imgs)
 del img_fnames
 del imgs
 
