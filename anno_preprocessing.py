72669a9d68301055b4b1fa968a2038438bc52cd6
kalibianw <kalibianw@gmail.com>
2022-04-25 02:32:38 +0900

    Modify anno_preprocessing.py to can select compress npz or not

--- anno_preprocessing.py
+++ anno_preprocessing.py
@@ -3,15 +3,22 @@ import numpy as np
 
 DATASET_DIR_PATH = "D:/AI/data/ASL Alphabet Synthetic"
 RESCALING_RATIO = 1
+COMPRESSED = False
 
 dm = AnnoDataModule(dataset_dir_path=DATASET_DIR_PATH)
 
 img_fnames, imgs = dm.img_to_np(rescaling_ratio=RESCALING_RATIO)
 print(img_fnames.shape, imgs.shape)
-np.savez_compressed(file=f"npz/AnnoImgNumpy_{RESCALING_RATIO}.npz", fnames=img_fnames, imgs=imgs)
+if COMPRESSED:
+    np.savez_compressed(file=f"npz/AnnoImgNumpy_{RESCALING_RATIO}.npz", fnames=img_fnames, imgs=imgs)
+else:
+    np.savez(file=f"npz/AnnoImgNumpy_{RESCALING_RATIO}.npz", fnames=img_fnames, imgs=imgs)
 del img_fnames
 del imgs
 
 label_fnames, chars, landmarks = dm.label_to_np(rescaling_ratio=RESCALING_RATIO)
 print(label_fnames.shape, chars.shape, landmarks.shape)
-np.savez_compressed(file=f"npz/AnnoLabelNumpy_{RESCALING_RATIO}.npz", fnames=label_fnames, chars=chars, landmarks=landmarks)
+if COMPRESSED:
+    np.savez_compressed(file=f"npz/AnnoLabelNumpy_{RESCALING_RATIO}.npz", fnames=label_fnames, chars=chars, landmarks=landmarks)
+else:
+    np.savez(file=f"npz/AnnoLabelNumpy_{RESCALING_RATIO}.npz", fnames=label_fnames, chars=chars, landmarks=landmarks)
