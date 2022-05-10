3cabe1269bc9c7e17368b277ca04cbd64f9b3d27
kalibianw <kalibianw@gmail.com>
2022-05-10 11:57:10 +0900

    Implement seg_visualization.py

---
+++ seg_visualization.py
@@ -0,0 +1,16 @@
+from utils import SegVisualModule
+
+import numpy as np
+
+npz_loader = np.load("npz/SegNumpy_0.5.npz")
+for key in npz_loader:
+    print(key)
+
+imgs = npz_loader["x_train"]
+chars = npz_loader["y_cls_train"]
+seg_imgs = npz_loader["y_seg_train"]
+print(imgs.shape, chars.shape, seg_imgs.shape)
+print(np.unique(chars, return_counts=True))
+
+svm = SegVisualModule(is_chars_normalized=False)
+svm.show_output(imgs[:5], chars[:5], seg_imgs[:5])
