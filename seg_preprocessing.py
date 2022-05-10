7d62a14c7843576c04f74756900253bddec87956
kalibianw <kalibianw@gmail.com>
2022-05-10 11:26:55 +0900

    Implement seg_preprocessing

---
+++ seg_preprocessing.py
@@ -0,0 +1,32 @@
+from utils import SegDataModule
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+DATASET_DIR_PATH = "D:/AI/data/ASL Alphabet Synthetic"
+RESCALING_RATIO = 0.5
+TEST_SIZE = 0.3
+COMPRESSED = False
+
+sdm = SegDataModule(dataset_dir_path=DATASET_DIR_PATH, rescaling_ratio=RESCALING_RATIO)
+
+img_fnames, imgs = sdm.img_to_np()
+print(img_fnames.shape, imgs.shape)
+
+label_fnames, chars, seg_imgs = sdm.label_to_np()
+print(label_fnames.shape, chars.shape, seg_imgs.shape)
+
+x_train, x_test, y_cls_train, y_cls_test, y_seg_train, y_seg_test = train_test_split(imgs, chars, seg_imgs, test_size=TEST_SIZE, stratify=chars)
+print(x_train.shape, x_test.shape,
+      y_cls_train.shape, y_cls_test.shape,
+      y_seg_train.shape, y_seg_test.shape)
+
+if COMPRESSED:
+    np.savez_compressed(file=f"npz/SegNumpy_{RESCALING_RATIO}_compressed.npz",
+                        x_train=x_train, x_test=x_test,
+                        y_cls_train=y_cls_train, y_cls_test=y_cls_test,
+                        y_seg_train=y_seg_train, y_seg_test=y_seg_test)
+else:
+    np.savez(file=f"npz/SegNumpy_{RESCALING_RATIO}.npz",
+             x_train=x_train, x_test=x_test,
+             y_cls_train=y_cls_train, y_cls_test=y_cls_test,
+             y_seg_train=y_seg_train, y_seg_test=y_seg_test)
