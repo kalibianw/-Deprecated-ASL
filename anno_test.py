2b29c28d5ea21a73c95f8cb8f07ba71e9c108614
kalibianw <kalibianw@gmail.com>
2022-05-23 00:24:29 +0900

    Change draw_point to draw_line on anno_test.py

--- anno_test.py
+++ anno_test.py
@@ -5,6 +5,7 @@ from sklearn.metrics import confusion_matrix
 
 import numpy as np
 import string
+import cv2
 
 MODEL_PATH = "model/anno_0.5.h5"
 NPZ_PATH = "npz/AnnoNumpy_0.5.npz"
@@ -24,6 +25,7 @@ model = models.load_model(MODEL_PATH)
 model.summary()
 model.evaluate(
     x={"img": x_test}, y={"cls_out": y_cls_test, "lndmrk_out": y_lndmrk_test},
+    verbose=1,
     batch_size=32
 )
 
@@ -37,7 +39,11 @@ print(cls_pred.shape)
 print(lndmrk_pred.shape)
 
 avm = AnnoVisualModule(is_chars_normalized=True, is_lndmrks_normalized=False)
-avm.draw_point(imgs=x_test[:5], chars=cls_pred[:5], lndmrks=lndmrk_pred[:5])
+avm.draw_line(img=x_test[0], lndmrk=lndmrk_pred[0], color=(255, 255, 255))
+
+cv2.imshow(f"{np.argmax(cls_pred[0])}", x_test[0])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 conf_mat = confusion_matrix(y_true=np.argmax(y_cls_test, axis=1), y_pred=np.argmax(cls_pred, axis=1))
 avm.plot_confusion_matrix(
